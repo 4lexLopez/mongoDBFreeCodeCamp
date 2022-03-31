13aca03ec92d4bd0479819d7e47a86641dda088c
4lexLopez <alex.roman.lopez@gmail.com>
2022-03-31 18:25:15 -0500

    Item 1: mongoose.connect function fixed

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 let myUri = process.env.MONGO_URI;
-mongoose.connect(myUri, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(myUri);
 
 let Person;
 
